0476f6b8c4ba503a84906b8f342ff065669aa200
aikanshagarwal <aikansh.agarwal@zemosolabs.com>
2022-04-21 21:29:29 +0530

    New commit 21stApril

---
+++ IdeaProjects/JavaLDP/src/LDPJava1/Assignment6Part2.java
@@ -0,0 +1,17 @@
+package LDPJava1;
+
+public class Assignment6Part2
+{
+    Assignment6Part2()
+    {
+        this(5,6);
+    }
+    Assignment6Part2(int a , int b)
+    {
+        System.out.print(a+b);
+    }
+    public static void main(String[] args)
+    {
+            Assignment6Part2 obj = new Assignment6Part2();
+    }
+}
---
+++ IdeaProjects/JavaLDP/src/LDPJava1/Assignment6Part3.java
@@ -0,0 +1,14 @@
+package LDPJava1;
+
+public class Assignment6Part3
+{
+    Assignment6Part3(String str)
+    {
+        System.out.println(str);
+    }
+    public static void main(String[] args)
+    {
+        Assignment6Part3[] obj = new Assignment6Part3[5];
+
+    }
+}
